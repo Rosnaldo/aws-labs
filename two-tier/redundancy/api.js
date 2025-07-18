32b0c284a568715917388b66cebe8c4b3acf2273
Andrey <andreytsuzuki@gmail.com>
2025-07-18 16:45:45 -0300

    first labs

---
+++ two-tier/redundancy/api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Parse JSON request bodies
+app.use(express.json());
+
+// Sample GET endpoint
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Sample POST endpoint
+app.post("/add", (req, res) => {
+  const data = req.body;
+  res.json({ you_sent: data });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
