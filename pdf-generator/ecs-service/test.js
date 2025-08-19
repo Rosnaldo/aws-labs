d2934c8b35565b9f081f70ee3e55d60f4043118d
Andrey <andreytsuzuki@gmail.com>
2025-08-19 15:05:37 -0300

    test path

--- pdf-generator/ecs-service/test.js
+++ pdf-generator/ecs-service/test.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const { join } = require('path')
-const { storeHtmlFileLocally } = require('./ecs-service/store-html-file-locally')
-const { generatePdf } = require('./ecs-service/generate-pdf')
-const { uploadFile } = require('./ecs-service/uploadFile')
-const { validateInput } = require('./ecs-service/validate')
+const { storeHtmlFileLocally } = require('./store-html-file-locally')
+const { generatePdf } = require('./generate-pdf')
+const { uploadFile } = require('./uploadFile')
+const { validateInput } = require('./validate')
 
 const filePath = __dirname
-const contentHtml = join(filePath, 'index.html')
+const contentHtml = join(filePath, '..', 'index.html')
 
 const title = '211-West'
 const decoded = fs.readFileSync(contentHtml).toString()
