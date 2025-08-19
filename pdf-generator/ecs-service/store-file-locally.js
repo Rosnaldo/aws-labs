f1ba4721613e3593609342c94b9c60798fbe64bf
Andrey <andreytsuzuki@gmail.com>
2025-08-19 10:44:26 -0300

    docker

--- pdf-generator/ecs-service/store-file-locally.js
+++ pdf-generator/ecs-service/store-file-locally.js
@@ -1,11 +1,8 @@
 const { writeFileSync } = require('fs')
 const { join } = require('path')
-const { execSync } = require('child_process')
 
 function storeFileLocally (file, name) {
   const pathFile = join('/var/lib', name)
-  console.log(execSync('whoami').toString())
-  console.log(execSync('ls -l /var/lib').toString())
   writeFileSync(pathFile, file)
   return pathFile
 }
